ecb8027b424024094c622524779d9b2141412e6b
Freddy Alvarado R <alvaguez1990@gmail.com>
2023-02-08 22:01:34 -0600

    feat: Fix card image

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { truncate } from "@/utils";
+
 type Props = {
   count: number;
   image: string;
@@ -9,16 +11,16 @@ type Props = {
 
 export function Card({ image, name, price, count }: Props) {
   return (
-    <article className="border">
+    <article className="flex flex-col items-center justify-between p-6 border-snow">
       <Image
-        className="object-cover"
-        width={80}
-        height={80}
+        className="object-contain h-32 w-32"
+        width={120}
+        height={120}
         src={image}
         alt={name}
       />
       <aside className="flex flex-col items-center mt-6">
-        <h3 className="text-lg">{name}</h3>
+        <h3 className="text-lg text-center">{truncate(name, 16)}</h3>
         <div className="flex justify-evenly w-full">
           <p>${price}</p>
           <p>{count} ⭐️</p>
--- frontend/src/components/gallery.tsx
+++ frontend/src/components/gallery.tsx
@@ -2,8 +2,6 @@ import { Card } from "@/components";
 
 import { getAllProducts } from "@/app/services";
 
-import { truncate } from "@/utils";
-
 type Props = {
   category: string;
 };
@@ -14,13 +12,13 @@ export async function Gallery({ category }: Props) {
   return (
     <section className="mx-auto w-3/4">
       <h2 className="py-8">{category}</h2>
-      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
         {products.map((product) => (
           <Card
             image={product.image}
             key={product.id}
             count={product.rating.count}
-            name={truncate(product.title, 22)}
+            name={product.title}
             price={product.price}
           />
         ))}
