2e69be5d1ec74c79b559c31354be9fb2f6f1a34c
Freddy Alvarado R <alvaguez1990@gmail.com>
2023-02-24 09:38:02 -0600

    feat: Add taildwind css to react ssr

--- frontend/react-ssr/src/entry-client.tsx
+++ frontend/react-ssr/src/entry-client.tsx
@@ -1,11 +1,12 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "~/App";
+
+import "~/index.css";
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById("root") as HTMLElement,
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
